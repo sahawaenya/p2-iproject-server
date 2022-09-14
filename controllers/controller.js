3b16c7f480cd67ca76bbfc4b630b0c620e50e5e3
sahawaenya <saha.waenya@gmail.com>
2022-09-14 11:22:54 +0700

    [DO]sympotm records

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,10 @@
-const { SymptomRecord, MedicalRecord, User, Disease } = require("../models");
+const {
+  SymptomRecord,
+  MedicalRecord,
+  User,
+  Disease,
+  Symptom,
+} = require("../models");
 const { Op } = require("sequelize");
 
 class Controller {
@@ -12,30 +18,34 @@ class Controller {
       let options = {};
       if (req.query.search) {
         options = {
-          [Op.and]: {
-            status: "Active",
+          [Op.or]: [
+            {
+              "$Doctor.name$": { [Op.iLike]: "%" + req.query.search + "%" },
+            },
+            {
+              "$Patient.name$": { [Op.iLike]: "%" + req.query.search + "%" },
+            },
 
-            [Op.or]: [
-              {
-                Doctor: { name: { [Op.iLike]: "%" + req.query.search + "%" } },
-              },
-              {
-                Patient: { name: { [Op.iLike]: "%" + req.query.search + "%" } },
-              },
-              {
-                Disease: { name: { [Op.iLike]: "%" + req.query.search + "%" } },
-              },
-            ],
-          },
+            {
+              "$Disease.name$": { [Op.iLike]: "%" + req.query.search + "%" },
+            },
+          ],
         };
       }
 
       let data = await MedicalRecord.findAndCountAll({
+        where: options,
         limit: dataPerPage,
         offset: dataFrom,
         include: [
-          { model: User, as: "Patient" },
-          { model: User, as: "Doctor" },
+          {
+            model: User,
+            as: "Patient",
+          },
+          {
+            model: User,
+            as: "Doctor",
+          },
           { model: Disease },
         ],
       });
@@ -55,6 +65,7 @@ class Controller {
           DiseaseDesc: el.Disease.description,
         };
       });
+
       data.total_data = data.count;
       data.current_page = +page;
       data.data_per_page = +dataPerPage;
@@ -63,6 +74,69 @@ class Controller {
       data.last_page = maxPage;
       data.data = data.rows;
       delete data.rows;
+      delete data.count;
+      res.status(200).json(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async getSymptRecord(req, res) {
+    try {
+      let { dataPerPage, page } = req.query;
+      if (!dataPerPage) dataPerPage = 10;
+      if (!page) page = 1;
+      const dataFrom = dataPerPage * (page - 1);
+      const dataTo = +dataFrom + +dataPerPage;
+      let options = {};
+      if (req.query.search) {
+        options = {
+          [Op.and]: [
+            {
+              "$SymptomRecords.SymptomId$": {
+                [Op.iLike]: "%" + req.query.search + "%",
+              },
+            },
+          ],
+        };
+      }
+
+      let data = await MedicalRecord.findAndCountAll({
+        limit: dataPerPage,
+        offset: dataFrom,
+        include: [
+          {
+            model: SymptomRecord,
+            where: { SymptomId: [71, 82] },
+          },
+        ],
+      });
+      const maxPage = Math.ceil(data.count / dataPerPage);
+
+      //   data.rows = data.rows.map((el) => {
+      //     return {
+      //       id: el.id,
+      //       DoctorId: el.DoctorId,
+      //       DoctorName: "dr. " + el.Doctor.name,
+      //       DoctorImage: el.Doctor.imageUrl,
+      //       PatientId: el.PatientId,
+      //       PatientName: el.Patient.name,
+      //       PatientImage: el.Patient.imageUrl,
+      //       DiseaseId: el.DiseaseId,
+      //       DiseaseName: el.Disease.name,
+      //       DiseaseDesc: el.Disease.description,
+      //     };
+      //   });
+
+      data.total_data = data.count;
+      data.current_page = +page;
+      data.data_per_page = +dataPerPage;
+      data.data_from = dataFrom + 1;
+      data.data_to = dataTo;
+      data.last_page = maxPage;
+      data.data = data.rows;
+      delete data.rows;
+      delete data.count;
       res.status(200).json(data);
     } catch (error) {
       console.log(error);
--- routes/medicalData.js
+++ routes/medicalData.js
@@ -3,5 +3,6 @@ const Controller = require("../controllers/controller");
 const routes = require("express").Router();
 
 routes.get("/medicalrecord", Controller.getMedicalRecord);
+routes.get("/symptomrecords", Controller.getSymptRecord);
 
 module.exports = routes;
